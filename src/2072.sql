f2d736379567b4509b8160291235cb20995f2825
muller2333 <zhxzhx070@hotmail.com>
2023-01-18 14:32:33 +0800

    2023-01-18

---
+++ src/2072.sql
@@ -0,0 +1,6 @@
+select if(n_score - c_score = 0, 'No Winner',
+          if(n_score - c_score > 0, 'New York University', 'California University')) winner
+from (select count(score) n_score, 1 id from NewYork where score >= 90) mid
+         inner join
+         (select count(score) c_score, 1 id from California where score >= 90) mid2
+         on mid2.id = mid.id;
--- src/2082.sql
+++ src/2082.sql
@@ -1,4 +1,3 @@
-# Write your MySQL query statement below
 select count(distinct customer_id) rich_count
 from Store
 where amount > 500;
