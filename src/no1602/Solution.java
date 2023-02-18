4a6c0623d6ab0b12b4b5ab668de00d4cf00c7f79
muller2333 <zhxzhx070@hotmail.com>
2023-02-18 20:28:12 +0800

    optimize 1602

--- src/no1602/Solution.java
+++ src/no1602/Solution.java
@@ -28,24 +28,59 @@ class Solution {
         queue.add(root);
         int size;
         while ((size = queue.size()) > 0) {
+            Queue<TreeNode> mid = new LinkedList<>();
+            boolean found = false;
             for (int i = 0; i < size; i++) {
                 TreeNode node = queue.poll();
                 if (node == u) {
-                    if (i == size - 1) {
-                        return null;
-                    } else {
-                        return queue.poll();
-                    }
+                    return i == size - 1 ? null : queue.poll();
                 } else {
                     TreeNode left = node.left;
                     if (left != null) {
+                        if (found) {
+                            return left;
+                        } else {
+                            if (left == u) {
+                                found = true;
+                            } else {
+                                TreeNode left2 = left.left;
+                                if (left2 != null) {
+                                    mid.add(left2);
+                                }
+                                TreeNode right2 = left.right;
+                                if (right2 != null) {
+                                    mid.add(right2);
+                                }
+                            }
+                        }
                         queue.add(left);
                     }
                     TreeNode right = node.right;
                     if (right != null) {
+                        if (found) {
+                            return right;
+                        } else {
+                            if (right == u) {
+                                found = true;
+                            } else {
+                                TreeNode left2 = right.left;
+                                if (left2 != null) {
+                                    mid.add(left2);
+                                }
+                                TreeNode right2 = right.right;
+                                if (right2 != null) {
+                                    mid.add(right2);
+                                }
+                            }
+                        }
                         queue.add(right);
                     }
                 }
+            }
+            if (found) {
+                return null;
+            } else {
+                queue = mid;
             }
         }
         return null;
