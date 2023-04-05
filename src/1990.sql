c1fbef9b75f03c9c318c740baa07a59b59f06078
muller2333 <zhxzhx070@hotmail.com>
2023-04-05 19:45:28 +0800

    2023-04-05

---
+++ src/1990.sql
@@ -0,0 +1,22 @@
+select mid2.platform, mid2.experiment_name, ifnull(cnt, 0) num_experiments
+from (select count(experiment_id) cnt, platform, experiment_name
+      from Experiments
+      group by platform, experiment_name) mid
+         right join (select 'IOS' platform, 'Programming' experiment_name
+                     union
+                     select 'IOS' platform, 'Sports' experiment_name
+                     union
+                     select 'IOS' platform, 'Reading' experiment_name
+                     union
+                     select 'Android' platform, 'Programming' experiment_name
+                     union
+                     select 'Android' platform, 'Sports' experiment_name
+                     union
+                     select 'Android' platform, 'Reading' experiment_name
+                     union
+                     select 'Web' platform, 'Programming' experiment_name
+                     union
+                     select 'Web' platform, 'Sports' experiment_name
+                     union
+                     select 'Web' platform, 'Reading' experiment_name) mid2
+                    on mid.platform = mid2.platform and mid.experiment_name = mid2.experiment_name;
