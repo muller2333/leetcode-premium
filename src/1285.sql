434c885dba53b567fbe5ca31c55b1403c5139e18
muller2333 <zhxzhx070@hotmail.com>
2023-04-06 15:06:25 +0800

    2023-04-06

---
+++ src/1285.sql
@@ -0,0 +1,20 @@
+select min(log_id) start_id, next_id end_id
+from (select log_id, min(next_id) next_id
+      from (
+               select Logs.log_id, next_id
+               from Logs
+                        left join (
+                   select log_id, next_id
+                   from (
+                            select log_id, if(log_id + 1 = next_id, null, log_id) next_id
+                            from (select log_id,
+                                         (select log_id from Logs where log_id > l.log_id order by log_id limit 1) next_id
+                                  from Logs l
+                                 ) mid
+                        ) mid2
+                   where next_id is not null
+               ) mid3
+                                  on Logs.log_id <= mid3.next_id
+           ) mid4
+      group by log_id) mid5
+group by next_id;
---
+++ src/574.sql
@@ -0,0 +1,3 @@
+select name
+from Candidate
+where id = (select candidateId from Vote group by candidateId order by count(id) desc limit 1);
