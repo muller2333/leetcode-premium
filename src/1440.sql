5b1cca0c252203e84c6a37e32901b5739fa266f1
muller2333 <zhxzhx070@hotmail.com>
2023-04-06 19:42:12 +0800

    2023-04-06

---
+++ src/1440.sql
@@ -0,0 +1,17 @@
+select left_operand, operator, right_operand, if(operator = res, 'true', 'false') value
+from (select left_operand,
+             mid3.operator,
+             right_operand,
+             if(mid2.value < mid3.value, '<', if(mid2.value = mid3.value, '=', '>')) res
+      from (select mid.left_operand, operator, value, r
+            from Variables
+                     right join
+                 (select left_operand, operator, ROW_NUMBER() over (order by operator) r from Expressions) mid
+                 on left_operand = name) mid2
+               inner join
+           (select mid.right_operand, operator, value, r
+            from Variables
+                     right join
+                 (select right_operand, operator, ROW_NUMBER() over (order by operator) r from Expressions) mid
+                 on right_operand = name) mid3
+           on mid2.r = mid3.r) mid4;
