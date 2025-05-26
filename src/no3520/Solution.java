aca3d4377379bb9f9e1f711bcb6cbae303ad93f6
muller2333 <zhxzhx070@hotmail.com>
2025-05-26 15:43:06 +0800

    update 1 solution

--- src/no3520/Solution.java
+++ src/no3520/Solution.java
@@ -7,17 +7,38 @@ import java.util.List;
 class Solution {
     public int minThreshold(int[] nums, int k) {
         int length = nums.length;
+        List<Integer> list = new ArrayList<>();
+        int[] indices = new int[length];
+        int sum = 0;
+        list.add(nums[length - 1]);
+        int diff = 0;
+        for (int i = length - 2; i >= 0; i--) {
+            int num = nums[i];
+            indices[i] = getRightmostIndex(list, num, 0);
+            sum += indices[i] + 1;
+            diff = Math.max(diff, num - list.get(0));
+            list.add(indices[i] + 1, num);
+        }
+        if (sum < k) {
+            return -1;
+        }
         int left = 1;
-        int right = 1_000_000_000;
+        int right = diff;
         while (left < right) {
             int mid = (right - left) / 2 + left;
-            List<Integer> list = new ArrayList<>();
+            list = new ArrayList<>();
             list.add(nums[length - 1]);
             int count = 0;
             for (int i = length - 2; i >= 0; i--) {
-                count += count(list, nums[i], mid);
-                if (count >= k) {
-                    break;
+                int num = nums[i];
+                if (num <= list.get(0)) {
+                    list.add(0, num);
+                } else {
+                    count += indices[i] - getRightmostIndex(list, num, mid) + 1;
+                    if (count >= k) {
+                        break;
+                    }
+                    list.add(indices[i] + 1, num);
                 }
             }
             if (count >= k) {
@@ -26,53 +47,25 @@ class Solution {
                 left = mid + 1;
             }
         }
-        return left == 1_000_000_000 ? -1 : left;
+        return left;
     }
 
-    public int count(List<Integer> list, int num, int threshold) {
-        int size = list.size();
-        int count = 0;
-        if (num > list.get(0) && num <= list.get(size - 1) + threshold) {
-            int search = Collections.binarySearch(list, num - 1);
-            int[] res = { -1, -1 };
-            if (search >= 0) {
-                int left = search;
-                int right = size - 1;
-                while (left <= right) {
-                    int mid = (left + right) >> 1;
-                    if (list.get(mid) == num - 1) {
-                        left = mid + 1;
-                    } else {
-                        right = mid - 1;
-                    }
+    public int getRightmostIndex(List<Integer> list, int num, int threshold) {
+        int index = Collections.binarySearch(list, num - threshold);
+        if (index >= 0) {
+            int left = 0;
+            while (left <= index) {
+                int mid = (left + index) >> 1;
+                if (list.get(mid) == num - threshold) {
+                    index = mid - 1;
+                } else {
+                    left = mid + 1;
                 }
-                res[1] = right;
-            } else {
-                res[1] = -search - 2;
             }
-            search = Collections.binarySearch(list, num - threshold);
-            if (search >= 0) {
-                int left = 0;
-                int right = search;
-                while (left <= right) {
-                    int mid = (left + right) >> 1;
-                    if (list.get(mid) == num - threshold) {
-                        right = mid - 1;
-                    } else {
-                        left = mid + 1;
-                    }
-                }
-                res[0] = left;
-            } else {
-                res[0] = -search - 1;
-            }
-            count = res[1] - res[0] + 1;
+            index++;
+        } else {
+            index = -index - 1;
         }
-        int search = Collections.binarySearch(list, num);
-        if (search < 0) {
-            search = -search - 1;
-        }
-        list.add(search, num);
-        return count;
+        return index - (threshold == 0 ? 1 : 0);
     }
 }
